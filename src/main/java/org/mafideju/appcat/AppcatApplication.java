5c882f342b755386b8286259c518a530de910092
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2023-08-30 19:38:49 -0300

    feat(ROOT): bootstrapping in Windows

---
+++ src/main/java/org/mafideju/appcat/AppcatApplication.java
@@ -0,0 +1,13 @@
+package org.mafideju.appcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcatApplication.class, args);
+	}
+
+}
