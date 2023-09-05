ca5d073721abbab9cc54342dc7e31f217d270ebf
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2023-09-05 17:34:08 -0300

    feat(APPLICATION.YAML): setting custom value props

--- src/main/java/org/mafideju/appcat/controller/BaseController.java
+++ src/main/java/org/mafideju/appcat/controller/BaseController.java
@@ -1,15 +1,22 @@
 package org.mafideju.appcat.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
+    @Value("${info.app.name}")
+    private String appName;
+    @Value("${info.app.description}")
+    private String appDesc;
 
     @GetMapping("/")
     public String BaseMethod() {
-        return "<div style='background-color: black; height: 100%; width: 100%; display: flex; align-items: center; justify-content: center;'>" +
-                "<h1 style='color: whitesmoke;'>Who Shot Biggie?</h1></div>";
+        System.out.println(appName);
+        return "<div style='background-color: black; height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;'>" +
+                "<div><h2 style='color: whitesmoke;'> " + appName + " </h2></div>" +
+                "<div><h2 style='color: whitesmoke;'> " + appDesc + "</h2></div></div>";
     }
 
     @GetMapping("/treino")
