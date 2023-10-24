947a6e484806cad7e415e31c9953d6ef2d061bd8
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2023-10-24 14:32:45 -0300

    feat(GEODATAREST): security

--- master/geodatarest/src/main/java/org/mars/geodatarest/GeodataRestApplication.java
+++ master/geodatarest/src/main/java/org/mars/geodatarest/GeodataRestApplication.java
@@ -3,7 +3,10 @@ package org.mars.geodatarest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"org.mars.geodatarest",
+		"org.mars.config"
+})
 public class GeodataRestApplication {
 
 	public static void main(String[] args) {
